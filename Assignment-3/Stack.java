3fc3a71501c0469333691f8787b419b6a9284a8f
Nagavignesh1729 <mnaga.1729@gmail.com>
2024-08-28 23:49:27 +0530

    feat: Implemented stack using Linked List

--- Assignment-3/Stack.java
+++ Assignment-3/Stack.java
@@ -1,2 +1,71 @@
-/* Need to implement Stack Data Structure in Java
-    push(), pop(), top() and display() */
+class Node {
+    int val;
+    Node next;
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class Stack {
+    private Node top;
+    private int size;
+
+    public Stack() {
+        top = null;
+        size = 0;
+    }
+    public void push(int val) {
+        Node newNode = new Node(val);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int value = top.val;
+        top = top.next;
+        size--;
+        return value;
+    }
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return top.val;
+    }
+    public boolean isEmpty() {
+        if(top == null)
+            return true;
+        return false;
+    }
+    public int size() {
+        return size;
+    }
+    public void displayStack() {
+        Node current = top;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.displayStack(); 
+
+        System.out.println("Top element: " + stack.peek());
+
+        System.out.println("Popped element: " + stack.pop());
+        stack.displayStack();
+
+        System.out.println("Stack size: " + stack.size());
+    }
+}
